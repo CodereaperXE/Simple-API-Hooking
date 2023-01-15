517d5d683b2d5f08ab6b6b346e4a5f1a4a268c2b
Rishab Budale <73216106+CodereaperXE@users.noreply.github.com>
2023-01-15 23:51:25 +0530

    Update apihooking.cpp

--- apihooking.cpp
+++ apihooking.cpp
@@ -6,16 +6,16 @@ using namespace std;
 void* function_addr;//Address where messageboxa lies
 void* proxy;//proxy function address
 CHAR original[5]={0};//saving the previous bytes of MessageBoxA to restore lated
-CHAR patch[5] = { 0 };
+CHAR patch[5] = {0};//buffer to write the jump offset
 DWORD *offset;//for jump instruction the offset between messageboxa address and proxy function address
 
 CHAR message[] = "Hello from Hooked function";//message to be printed by hooked function
 CHAR caption[] = "Hooked Function";//message to be printed by hooked function 
 
-int __stdcall hook(HWND hWnd,LPCSTR lpText,LPCSTR lpCaption,UINT uType) { //stdcall immitates MessageBoxA stdcall its also the proxyfunction
+int __stdcall hook(HWND hWnd,LPCSTR lpText,LPCSTR lpCaption,UINT uType) { //stdcall immitates MessageBoxA stdcall, its also the proxyfunction
 	cout << "Hook attached" << endl;
-	cout <<"Text is " << (LPCSTR)lpText << endl;
-	cout << "Caption is " << (LPCSTR)lpCaption << endl;
+	cout <<"Text is " << (LPCSTR)lpText << endl; //printing data from the messageboxa when it was called 
+	cout << "Caption is " << (LPCSTR)lpCaption << endl;//printing data from the messageboxa when it was called
 	WriteProcessMemory(GetCurrentProcess(), (LPVOID)function_addr, original, 5, NULL);//unhooking by writing the original of messageboxa 
 	cout << "Unhooking" << endl;
 	MessageBoxA(NULL, (LPCSTR)message, (LPCSTR)caption, MB_OK);//calling unhooked messageboxa with custom data
