770138d276cbafc75b269b8ea683f0b79f6d765d
Rishab Budale <73216106+CodereaperXE@users.noreply.github.com>
2023-01-15 23:48:12 +0530

    Update apihooking.cpp

--- apihooking.cpp
+++ apihooking.cpp
@@ -1,40 +1,39 @@
 #include <iostream>
-#include <Windows.h>
+#include <Windows.h>//to access winapi
 using namespace std;
 
 
-char saved_bytes[4];
-void* function_addr;
-void* proxy;
-CHAR original[5]={0};
+void* function_addr;//Address where messageboxa lies
+void* proxy;//proxy function address
+CHAR original[5]={0};//saving the previous bytes of MessageBoxA to restore lated
 CHAR patch[5] = { 0 };
-DWORD *offset;
+DWORD *offset;//for jump instruction the offset between messageboxa address and proxy function address
 
-CHAR message[] = "Hello from Hooked function";
-CHAR caption[] = "Hooked Function";
+CHAR message[] = "Hello from Hooked function";//message to be printed by hooked function
+CHAR caption[] = "Hooked Function";//message to be printed by hooked function 
 
-int __stdcall hook(HWND hWnd,LPCSTR lpText,LPCSTR lpCaption,UINT uType) {
+int __stdcall hook(HWND hWnd,LPCSTR lpText,LPCSTR lpCaption,UINT uType) { //stdcall immitates MessageBoxA stdcall its also the proxyfunction
 	cout << "Hook attached" << endl;
 	cout <<"Text is " << (LPCSTR)lpText << endl;
 	cout << "Caption is " << (LPCSTR)lpCaption << endl;
-	WriteProcessMemory(GetCurrentProcess(), (LPVOID)function_addr, original, 5, NULL);
+	WriteProcessMemory(GetCurrentProcess(), (LPVOID)function_addr, original, 5, NULL);//unhooking by writing the original of messageboxa 
 	cout << "Unhooking" << endl;
-	MessageBoxA(NULL, (LPCSTR)message, (LPCSTR)caption, MB_OK);
+	MessageBoxA(NULL, (LPCSTR)message, (LPCSTR)caption, MB_OK);//calling unhooked messageboxa with custom data
 	return 0;
 }
 
 int insert_hook() {
-	HINSTANCE ins = LoadLibraryA("user32.dll");
+	HINSTANCE ins = LoadLibraryA("user32.dll"); //creating instance of user32.dll
 
-	function_addr = GetProcAddress(ins, "MessageBoxA");
-	ReadProcessMemory(GetCurrentProcess(), function_addr, original, 5, NULL);
-	proxy = &hook;
-	offset = (DWORD*)((DWORD)proxy - ((DWORD)function_addr + 5));
+	function_addr = GetProcAddress(ins, "MessageBoxA"); //getting messageboxa address
+	ReadProcessMemory(GetCurrentProcess(), function_addr, original, 5, NULL);//reading first 5 bytes of messageboxa to restore after unhooking
+	proxy = &hook;//proxy function address
+	offset = (DWORD*)((DWORD)proxy - ((DWORD)function_addr + 5));//calculating offset for jump instruction
 
-	memcpy(patch, "\xE9", 1);
-	memcpy(patch + 1, &offset, 4);
+	memcpy(patch, "\xE9", 1);//moving jmp
+	memcpy(patch + 1, &offset, 4);//moving offset
 
-	WriteProcessMemory(GetCurrentProcess(), (LPVOID)function_addr, patch, 5, NULL);
+	WriteProcessMemory(GetCurrentProcess(), (LPVOID)function_addr, patch, 5, NULL);//writing proxy function offset into address of messageboxa
 	
 	return 0;
 }
@@ -42,7 +41,7 @@ int main() {
 	
 	MessageBoxA(NULL, "Hello", "Hello from function", MB_OK);
 
-	insert_hook();
+	insert_hook();//inserting hook
 
 	MessageBoxA(NULL, "Hello ", "Hijacked", MB_OK);
 
